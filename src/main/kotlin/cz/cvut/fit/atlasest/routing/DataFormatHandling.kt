dad36544ea282d5a4809c4ce868b74cab995e091
Ester Atlasová <atlasest@fit.cvut.cz>
2025-04-01 13:06:55 +0200

    feat: content negotiation

--- src/main/kotlin/cz/cvut/fit/atlasest/routing/DataFormatHandling.kt
+++ src/main/kotlin/cz/cvut/fit/atlasest/routing/DataFormatHandling.kt
@@ -4,7 +4,9 @@ import cz.cvut.fit.atlasest.utils.csvToJson
 import cz.cvut.fit.atlasest.utils.jsonToCsv
 import cz.cvut.fit.atlasest.utils.toXML
 import cz.cvut.fit.atlasest.utils.xmlToJson
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.parseAndSortContentTypeHeader
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.response.respond
@@ -18,23 +20,27 @@ val XML_MIME = "application/xml"
 val CSV_MIME = "text/csv"
 val ALL_MIME = "*/*"
 
+val SUPPORTED_TYPES = listOf(ContentType.Application.Json, ContentType.Application.Xml, ContentType.Text.CSV)
+
 fun getResourceInJsonFormat(
     body: String,
-    contentType: String,
+    contentTypeValue: String,
     convertJson: (JsonObject) -> JsonObject,
-): JsonObject =
-    when (contentType) {
-        JSON_MIME ->
+): JsonObject {
+    val contentType = ContentType.parse(contentTypeValue)
+    return when (contentType) {
+        ContentType.Application.Json ->
             try {
                 Json.parseToJsonElement(body).jsonObject
             } catch (e: Exception) {
                 throw BadRequestException("Problem when parsing JSON body (must be a JSON object)", e)
             }
 
-        XML_MIME -> {
+        ContentType.Application.Xml -> {
             convertJson(xmlToJson(body))
         }
-        CSV_MIME -> {
+
+        ContentType.Text.CSV -> {
             convertJson(csvToJson(body))
         }
 
@@ -42,26 +48,58 @@ fun getResourceInJsonFormat(
             "Unsupported content type $contentType. Supported types are: [$JSON_MIME, $XML_MIME, $CSV_MIME]",
         )
     }
+}
 
 suspend fun returnResourceInAcceptedFormat(
     call: ApplicationCall,
     code: HttpStatusCode,
     resource: JsonElement,
     accept: String,
-) = when (accept) {
-    JSON_MIME, ALL_MIME -> {
-        call.response.headers.append("Content-Type", JSON_MIME)
-        call.respond(code, resource)
+) {
+    val acceptNegotiated = negotiateContent(accept)
+
+    when (acceptNegotiated) {
+        ContentType.Application.Json -> {
+            call.response.headers.append("Content-Type", JSON_MIME)
+            call.respond(code, resource)
+        }
+        ContentType.Application.Xml -> {
+            call.response.headers.append("Content-Type", XML_MIME)
+            call.respond(code, resource.toXML())
+        }
+        ContentType.Text.CSV -> {
+            call.response.headers.append("Content-Type", CSV_MIME)
+            call.respond(code, jsonToCsv(resource))
+        }
+
+        else -> throw BadRequestException(
+            "Unsupported accept type $accept. Supported types are: [$JSON_MIME, $XML_MIME, $CSV_MIME]",
+        )
     }
-    XML_MIME -> {
-        call.response.headers.append("Content-Type", XML_MIME)
-        call.respond(code, resource.toXML())
+}
+
+internal fun negotiateContent(accept: String): ContentType? {
+    if (accept.isBlank()) {
+        return SUPPORTED_TYPES.first()
     }
-    CSV_MIME -> {
-        call.response.headers.append("Content-Type", CSV_MIME)
-        call.respond(code, jsonToCsv(resource))
-    }
-    else -> throw BadRequestException(
-        "Unsupported accept type $accept. Supported types are: [$JSON_MIME, $XML_MIME, $CSV_MIME]",
-    )
+    val acceptParsed = parseAndSortContentTypeHeader(accept)
+
+    val (disallowedTypes, allowedTypes) =
+        acceptParsed
+            .partition { it.quality == 0.0 }
+            .let { (disallowed, allowed) ->
+                disallowed.map { ContentType.parse(it.value) } to allowed.map { ContentType.parse(it.value) }
+            }
+    val allowedSupportedTypes = SUPPORTED_TYPES.filterNot { it in disallowedTypes }
+
+    val result =
+        allowedTypes.firstNotNullOfOrNull { accepted ->
+            allowedSupportedTypes.firstOrNull { supported ->
+                (accepted.contentType == supported.contentType && accepted.contentSubtype == supported.contentSubtype) ||
+                    (accepted.contentType == supported.contentType && accepted.contentSubtype == "*") ||
+                    (accepted.contentType == "*" && accepted.contentSubtype == "*")
+            }
+        }
+
+    return result
 }
--- src/test/kotlin/cz/cvut/fit/atlasest/routing/DataFormatHandlingTest.kt
+++ src/test/kotlin/cz/cvut/fit/atlasest/routing/DataFormatHandlingTest.kt
@@ -1,5 +1,6 @@
 package cz.cvut.fit.atlasest.routing
 
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.plugins.BadRequestException
@@ -141,4 +142,100 @@ class DataFormatHandlingTest {
                 }
             assertEquals("Unsupported accept type text/html. Supported types are: [$JSON_MIME, $XML_MIME, $CSV_MIME]", exception.message)
         }
+
+    @Test
+    fun `negotiateContent - when CSV has higher priority than all application types - returns CSV`() {
+        val mediaType = negotiateContent("text/csv, application/*; q=0.9")
+        assertEquals(ContentType.Text.CSV, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when accepts all application types - returns JSON`() {
+        val mediaType = negotiateContent("application/*")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when non supported types are given - returns null`() {
+        val mediaType = negotiateContent("text/html, text/plain")
+        assertEquals(null, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when accepts explicitly XML and all application types - returns XML`() {
+        val mediaType = negotiateContent("application/*, application/xml") // application/xml will have higher priority
+        assertEquals(ContentType.Application.Xml, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when all supported types are accepted - returns JSON`() {
+        val mediaType = negotiateContent("application/json, application/xml, text/csv")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when JSON has highest priority - returns JSON`() {
+        val mediaType = negotiateContent("application/json; q=0.9, application/xml; q=0.8, text/csv; q=0.6")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when XML has highest priority - returns XML`() {
+        val mediaType = negotiateContent("application/json; q=0.5, application/xml; q=0.9, text/csv; q=0.7")
+        assertEquals(ContentType.Application.Xml, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when CSV has highest priority - returns CSV`() {
+        val mediaType = negotiateContent("application/json; q=0.4, application/xml; q=0.5, text/csv; q=0.9")
+        assertEquals(ContentType.Text.CSV, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when any application type is preferred - returns JSON`() {
+        val mediaType = negotiateContent("application/*, text/csv; q=0.9")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when any text type is preferred - returns CSV`() {
+        val mediaType = negotiateContent("text/*, application/*; q=0.9")
+        assertEquals(ContentType.Text.CSV, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when wildcard is used - returns JSON`() {
+        val mediaType = negotiateContent("*/*")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when wildcard is used but JSON is excluded - returns XML`() {
+        val mediaType = negotiateContent("*/*, application/json; q=0")
+        assertEquals(ContentType.Application.Xml, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when wildcard is used but JSON and XML is excluded - returns CSV`() {
+        val mediaType = negotiateContent("*/*, application/json; q=0, application/xml; q=0")
+        assertEquals(ContentType.Text.CSV, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when unsupported media type is preferred, but a supported type is included - returns JSON`() {
+        val mediaType = negotiateContent("image/png, application/json; q=0.9")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when no Accept header is present - returns JSON`() {
+        val mediaType = negotiateContent("")
+        assertEquals(ContentType.Application.Json, mediaType)
+    }
+
+    @Test
+    fun `negotiateContent - when all formats are explicitly rejected - returns null`() {
+        val mediaType = negotiateContent("application/json; q=0, application/xml; q=0, text/csv; q=0")
+        assertEquals(null, mediaType)
+    }
 }
